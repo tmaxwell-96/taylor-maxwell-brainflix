616459bb0691add03f39be2c777a70a75b2671fb
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-10-22 12:17:33 -0700

    Complete styling to tablet mockup

--- src/components/VideoListCard/VideoListCard.jsx
+++ src/components/VideoListCard/VideoListCard.jsx
@@ -9,19 +9,19 @@ const VideoListCard = (props) => {
         return props.changeVideo(props.video.id);
         // return console.log(props.video.id);
       }}
-      className="videolist__card"
+      className="videolistcard"
     >
-      <div className="videolist__image-container">
+      <div className="videolistcard__image-container">
         <img
-          className="videolist__image"
+          className="videolistcard__image"
           src={props.video.image}
           alt={props.video.title}
         />
       </div>
 
-      <div className="videolist__details">
-        <p className="videolist__title">{props.video.title}</p>
-        <p className="videolist__channel">{props.video.channel}</p>
+      <div className="videolistcard__details">
+        <p className="videolistcard__title">{props.video.title}</p>
+        <p className="videolistcard__channel">{props.video.channel}</p>
       </div>
     </div>
   );
