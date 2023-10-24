957c06cce9261c6551200b963610858ac17c9902
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-10-24 14:57:42 -0700

    Add home and upload page, add react router

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import "./HomePage.scss";
+import videoDetailsSimple from "../../data/videos.json";
+import videoDetails from "../../data/video-details.json";
+import Video from "../../components/Video/Video";
+import Comments from "../../components/Comments/Comments";
+import VideoList from "../../components/VideoList/VideoList";
+import VideoDetails from "../../components/VideoDetails/VideoDetails";
+
+function HomePage() {
+  const [videoData, setVideoData] = useState(videoDetails);
+  // setVideoData never used as awaiting dynamic population in other sprints
+
+  const [selectedVideo, setSelectedVideo] = useState(videoData[0]);
+
+  // Event handler select video function
+  //----------------------------------------
+
+  function changeVideo(displayedVideoId) {
+    const foundVideo = videoDetails.find(
+      (videoObject) => videoObject.id === displayedVideoId
+    );
+    setSelectedVideo(foundVideo);
+  }
+
+  // Dynamic Timestamp Function
+  //----------------------------------------
+
+  function calculateTimeAgo(timestamp) {
+    const currentDate = new Date();
+    const commentDate = new Date(timestamp);
+
+    const timeDifference = currentDate - commentDate;
+
+    const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+    const hoursAgo = Math.floor(minutesAgo / 60);
+    const daysAgo = Math.floor(hoursAgo / 24);
+    const monthsAgo = Math.floor(daysAgo / 30.437); //average number of days in a month
+    const yearsAgo = Math.floor(monthsAgo / 12);
+
+    if (yearsAgo > 0) {
+      if (yearsAgo === 1) {
+        return `${yearsAgo} Year Ago`;
+      } else {
+        return `${yearsAgo} Years Ago`;
+      }
+    } else if (monthsAgo > 0) {
+      if (monthsAgo === 1) {
+        return `${monthsAgo} Month Ago`;
+      } else {
+        return `${monthsAgo} Months Ago`;
+      }
+    } else if (daysAgo > 0) {
+      if (daysAgo === 1) {
+        return `${daysAgo} Day Ago`;
+      } else {
+        return `${daysAgo} Days Ago`;
+      }
+    } else if (hoursAgo > 0) {
+      if (hoursAgo === 1) {
+        return `${hoursAgo} Hour Ago`;
+      } else {
+        return `${hoursAgo} Hours Ago`;
+      }
+    } else if (minutesAgo > 0) {
+      if (minutesAgo === 1) {
+        return `${minutesAgo} Minute Ago`;
+      } else {
+        return `${minutesAgo} Minutes Ago`;
+      }
+    } else {
+      return `Just Now`;
+    }
+  }
+
+  return (
+    <>
+      <Video selectedVideo={selectedVideo} />
+
+      <div className="details">
+        <div className="details__left">
+          <VideoDetails
+            videoDetails={videoDetails}
+            selectedVideo={selectedVideo}
+            calculateTimeAgo={calculateTimeAgo}
+          />
+
+          <Comments
+            calculateTimeAgo={calculateTimeAgo}
+            videoDetails={videoDetails}
+            selectedVideo={selectedVideo}
+          />
+        </div>
+        <div className="details__right">
+          <VideoList
+            videoDetailsSimple={videoDetailsSimple}
+            changeVideo={changeVideo}
+            selectedVideo={selectedVideo}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/Upload/UploadPage.jsx
@@ -0,0 +1,52 @@
+import "./UploadPage.scss";
+import thumbnail from "../../assets/images/Upload-video-preview.jpg";
+import uploadIcon from "../../assets/images/Icons/upload.svg";
+
+const UploadPage = () => {
+  return (
+    <div className="upload">
+      <h2 className="upload__header">Upload Video</h2>
+      <div className="upload__thumbnail-container">
+        <h3 className="upload__label">Video Thumbnail</h3>
+        <img
+          className="upload__thumbnail"
+          src={thumbnail}
+          alt="upload image thumbnail"
+        />
+      </div>
+      <form className="upload__form">
+        <label className="upload__label" htmlFor="title">
+          {" "}
+          TITLE YOUR VIDEO
+          <input
+            name="title"
+            type="text"
+            placeholder="Add a title to your video"
+          />
+        </label>
+        <label className="upload__label" htmlFor="title">
+          {" "}
+          ADD A VIDEO DESCRIPTION
+          <textarea
+            name="description"
+            type="text"
+            placeholder="Add a description of your video"
+          />
+        </label>
+        <div className="nav__buttonwrapper">
+          <div className="nav__button-overlay">
+            <img
+              className="nav__upload-icon"
+              src={uploadIcon}
+              alt="upload icon"
+            />
+          </div>
+          <button className="nav__button">PUBLISH</button>
+        </div>
+        <h3 className="upload__cancel">CANCEL</h3>
+      </form>
+    </div>
+  );
+};
+
+export default UploadPage;
