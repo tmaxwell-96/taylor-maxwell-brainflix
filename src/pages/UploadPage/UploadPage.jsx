a60eff953a1e20ae476031f8a02ca17feb940b56
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-10-26 17:06:12 -0700

    Style upload page for all sizes

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,63 +1,64 @@
 import "./UploadPage.scss";
 import thumbnail from "../../assets/images/Upload-video-preview.jpg";
 import uploadIcon from "../../assets/images/Icons/upload.svg";
+import { Link } from "react-router-dom";
 
 const UploadPage = () => {
   return (
     <div className="upload">
       <h2 className="upload__header">Upload Video</h2>
-      <div className="upload__thumbnail-container">
-        <h3 className="upload__label">Video Thumbnail</h3>
-        <img
-          className="upload__thumbnail"
-          src={thumbnail}
-          alt="upload  thumbnail"
-        />
-      </div>
-      <form className="form">
-        <div className="form__right">
-          <div className="form__comment-wrapper">
-            <label
-              htmlFor="form__comment"
-              name="commentText"
-              className="form__label"
-            >
-              TITLE YOUR VIDEO
-            </label>
-            <textarea
-              className="form__title"
-              name="commentFormText"
-              id="form__comment"
-              placeholder="Add a title to your video"
-            ></textarea>
-            <label
-              htmlFor="form__comment"
-              name="commentText"
-              className="form__label"
-            >
-              ADD A VIDEO DESCRIPTION
-            </label>
-            <textarea
-              className="form__comment"
-              name="commentFormText"
-              id="form__comment"
-              placeholder="Add a Video Description"
-            ></textarea>
-          </div>
 
-          <div className="form__buttonwrapper">
-            <div className="form__button-overlay">
+      <form className="uploadform">
+        <div className="uploadform__top">
+          <div className="uploadform__container">
+            <h3 className="uploadform__label">VIDEO THUMBNAIL</h3>
+            <img
+              className="uploadform__thumbnail"
+              src={thumbnail}
+              alt="upload  thumbnail"
+            />
+          </div>
+          <div className="uploadform__inputs">
+            <label className="uploadform__label">TITLE YOUR VIDEO </label>
+            <input
+              className="uploadform__title"
+              type="text"
+              placeholder="Add a title to your video"
+            />
+
+            <label className="uploadform__label">
+              ADD A VIDEO DESCRIPTION{" "}
+            </label>
+            <textarea
+              className="uploadform__description"
+              type="text"
+              placeholder="Add a description to your video"
+            />
+          </div>
+        </div>
+
+        <div className="uploadform__bottom">
+          <h3 className="uploadform__cancel uploadform__cancel--wide">
+            CANCEL
+          </h3>
+          <div className="uploadform__buttonwrapper">
+            <div className="uploadform__button-overlay">
               <img
-                className="form__upload-icon"
+                className="uploadform__upload-icon"
                 src={uploadIcon}
                 alt="upload icon"
               />
             </div>
-            <button className="form__button">PUBLISH</button>
+            <button className="uploadform__button">PUBLISH</button>
           </div>
+          <Link to="/">
+            {" "}
+            <h3 className="uploadform__cancel uploadform__cancel--mobile">
+              CANCEL
+            </h3>
+          </Link>
         </div>
       </form>
-      <h3 className="upload__cancel">CANCEL</h3>
     </div>
   );
 };
